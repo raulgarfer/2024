52fb08d31d459f7219539fa69e664b34c3fb1d5a
raul garcia <raulgarfer@gmail.com>
2023-11-29 20:23:11 +0100

    clean

---
+++ src/sprites/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset" tilewidth="8" tileheight="8" tilecount="12" columns="4">
+ <image source="tileset.png" width="32" height="24"/>
+</tileset>
